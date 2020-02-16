6b43d7ee2a3b73ceb7444d00aff55ef003769ee9
edonadini <43779349+edonadini@users.noreply.github.com>
2020-02-16 21:56:03 +0100

    ho messo chunk nella funzione ma ora ho un out of bound

--- src/pgutils.py
+++ src/pgutils.py
@@ -59,7 +59,9 @@ def update_landmarks(songs, chunk, distances, params):
 # Ad ogni canzone nel dataset viene associata il landmark piu vicino ed i sucessori
 # osservati nelle playlist
 
-def initialize_landmarks(songs, params, x, chunk):
+def initialize_landmarks(songs, params, x, transition_matrix):
+    chunk = [[i for i in range(songs) if transition_matrix[s][i] > 0] for s in range(songs)]
+
     dim = len(x)
     distance_mat = [[np.linalg.norm(x[i] - x[j]) for j in range(dim)] for i in range(dim)]
     initial_distance = np.array(distance_mat).reshape((dim, dim))
@@ -73,7 +75,7 @@ def initialize_landmarks(songs, params, x, chunk):
         while len(chunk[s]) / songs < params.r:
             # mi sa che devo considerare non Z ma delta perché voglio la distanza tra le canzoni
             # Z indica la partition function
-            closest_idx = np.argmin(np.array([initial_distance[s, j] for j in landmarks]))
+            closest_idx = np.argmin(np.array([initial_distance[s][j] for j in landmarks]))
             closest_landmark = landmarks[closest_idx]
 
             if closest_landmark not in chunk[s]:
